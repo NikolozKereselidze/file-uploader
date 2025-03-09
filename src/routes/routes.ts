7841cf4004d2246d0456c36f7c0f08e88d3c6ab6
Nikusha <nikusha@Nikas-MacBook-Pro.local>
2025-03-09 20:28:27 +0400

    feat: add file upload and logout functionality

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,11 +2,8 @@ import express, { Request, Response } from "express";
 import { createUser, loginUser } from "../controllers/userController";
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  {
-    res.render("index");
-  }
-});
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router.get("/signup", (req: Request, res: Response) => {
   {
@@ -20,5 +17,30 @@ router.get("/login", (req: Request, res: Response) => {
   res.render("login");
 });
 router.post("/login", loginUser);
+
+router.post("/upload", upload.single("file"), (req: Request, res: Response) => {
+  console.log("done"); // The uploaded file information
+  res.redirect("/");
+});
+
+router.post("/logout", function (req, res, next) {
+  console.log("hello");
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+router.get("/", (req: Request, res: Response) => {
+  {
+    if (req.user) {
+      res.render("index");
+    } else {
+      res.redirect("/login");
+    }
+  }
+});
 
 module.exports = router;
